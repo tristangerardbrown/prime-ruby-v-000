2bf4f1a461e001fa457ad706b3fd839cbffeb0ba
Tristan Brown <tgbrown@stanford.edu>
2018-08-23 20:26:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime? (integer)
   if integer < 2
     return false
   end
-  (2..integer - 1)to_a.each do |x|
+  (2..integer - 1).to_a.each do |x|
    if (integer % x) == 0
     return false
   end
